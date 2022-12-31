06819f7ec7b60b11353daee666b7995761476cf3
dantsyt <116282387+dantsyt@users.noreply.github.com>
2022-12-31 05:19:02 +0200

    no script() for hover:none

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,7 +2,6 @@ const logo = document.querySelector('#logo');
 const navbar = document.querySelector('#navbar');
 const navBtns = document.querySelectorAll('.nav-btns');
 
-// Add comment
 const displayNavBar = () => {
     navbar.className = 'navbar';
     for (let one of navBtns) {
@@ -10,30 +9,26 @@ const displayNavBar = () => {
     };
 };
 
-logo.addEventListener('mouseenter', displayNavBar);
 
-logo.addEventListener('mouseleave', () => {
-    setTimeout(() => {
-        navbar.className = 'navbar_hidden';
-        navBtns.className = 'nav-btns_hidden';
-    }, 4000);
-    setTimeout(() => {
-        for (let one of navBtns) {
-            one.style.visibility = 'hidden';
-        };
-    }, 6000);
-});
 
-const paragraph = document.querySelector('h1');
+const h1Test = document.querySelector('h1');
 const mediaQueryList = window.matchMedia('(hover:none)');
 
 
 if (mediaQueryList.matches) {
-    /* the viewport is 600 pixels wide or less */
-    paragraph.textContent = 'This.';
-    document.body.style.backgroundColor = 'pink';
+
 } else {
-    /* the viewport is more than 600 pixels wide */
-    paragraph.textContent = 'This is a wide screen — more than 600px wide.';
-    document.body.style.backgroundColor = 'aquamarine';
-}
+    logo.addEventListener('mouseenter', displayNavBar);
+
+    logo.addEventListener('mouseleave', () => {
+        setTimeout(() => {
+            navbar.className = 'navbar_hidden';
+            navBtns.className = 'nav-btns_hidden';
+        }, 4000);
+        setTimeout(() => {
+            for (let one of navBtns) {
+                one.style.visibility = 'hidden';
+            };
+        }, 6000);
+    });
+};
