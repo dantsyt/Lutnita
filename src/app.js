eb7054a8e47e2f11329aa6f56e3d30328981c527
Constantin <dantsyt@gmail.com>
2023-09-08 23:36:17 +0300

    port change

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path = require('node:path');
 const express = require('express');
 const hbs = require('hbs');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const app = express();
 
