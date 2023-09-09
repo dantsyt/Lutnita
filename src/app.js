dcf596db0eb9c845c0b624b08326eeb857f879e9
dantsyt <dantsyt@gmail.com>
2023-09-09 18:05:46 +0300

    latest changes

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ app.set('view engine', 'hbs');
 app.set('views', templatesPath);
 hbs.registerPartials(partialsPath);
 
+// HTTPs redirect
+
+// app.use((req, res, next) => {
+//     if (req.header('x-forwarded-proto') !== 'https')
+//       res.redirect(`https://${req.header('host')}${req.url}`)
+//     else
+//       next()
+//   })
+
 // Set static directory
 
 app.use(express.static(publicPath));
