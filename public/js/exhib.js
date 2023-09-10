1f5d6d04f64ab3d0b0b05d9f9ba3672b320f7c25
dantsyt <dantsyt@gmail.com>
2023-09-10 23:44:28 +0300

    exhib img load

--- public/js/exhib.js
+++ public/js/exhib.js
@@ -3,6 +3,6 @@ const image = document.querySelector('.center_image_exhib');
 
 for (let one of names) {
     one.addEventListener('mouseenter', () => {
-        image.src = `http://localhost:3000/img/${one.id}.png`
+        image.src = `img/${one.id}.png`
     })
 }
