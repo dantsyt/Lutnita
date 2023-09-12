d30d613a8d3e0c291f6248ba4b06a755aa330369
dantsyt <dantsyt@gmail.com>
2023-09-13 01:24:11 +0300

    DB env key

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://dantsyt:BouDaQLLpbRFjNeJ@cluster0.49kxau7.mongodb.net/lutnita?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGODB_URI)
 
