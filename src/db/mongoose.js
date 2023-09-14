55d24c7034f6cdf156a53b8c7191faa39eeb5231
dantsyt <dantsyt@gmail.com>
2023-09-14 21:45:12 +0300

    mongodb_uri

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dantsyt:a80FKUYCEoWXeVFA@cluster0.49kxau7.mongodb.net/lutnita?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGODB_URI)
 
