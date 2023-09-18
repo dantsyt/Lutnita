cb4356ee99d453274163257a5f837ec8b276a330
dantsyt <dantsyt@gmail.com>
2023-09-19 01:08:10 +0300

    onclick image change desktop

--- public/js/oneExhib.js
+++ public/js/oneExhib.js
@@ -1,16 +1,16 @@
-const title = document.querySelector('#title');
-const exhName = title.innerText.toLowerCase();
-const imgContainer = document.querySelector('.image_container');
+const title = document.querySelector('#title')
+const exhName = title.innerText.toLowerCase()
+const imgContainer = document.querySelector('.image_container')
 
 // for phones - images as array in DB and loop OR get to separate hbs
 
 async function getOneExhib(exh) {
     try {
-        const response = await fetch(`/getOneExhib/${exh}`);
-        const oneExhib = await response.json();
-        imgArr = oneExhib.imgpath;
-        captionsArr = oneExhib.captions;
-        const countTotal = imgArr.length;
+        const response = await fetch(`/getOneExhib/${exh}`)
+        const oneExhib = await response.json()
+        imgArr = oneExhib.imgpath
+        captionsArr = oneExhib.captions
+        const countTotal = imgArr.length
         title.insertAdjacentHTML('afterend', `
         <div id="${oneExhib.exhibname}" class="text_container_nohover">
         <h3 class="first_name">${oneExhib.firstname}</h3>
@@ -21,40 +21,77 @@ async function getOneExhib(exh) {
         <p id="counter_text" class="counters">Exhibition views</p>
         <p class="counters"><span id="counter_num"> 1</span>/<span id="counter_total">${countTotal}</span></p>
         </div>
-        `);
-        const container = document.querySelector('.text_container_nohover');
+        `)
         for (let i = 0; i < imgArr.length; i++) {
             imgContainer.insertAdjacentHTML('beforeend', `
         <div class="image_container_mob_one">
         <img class="center_image_mob" src="${imgArr[i]}" alt="center_image">
         <p class="captions">${captionsArr[i].replace(/\\n/g, '<br>')}</p>
         </div>
-            `);
-        };
+            `)
+        }
         imgContainer.insertAdjacentHTML('afterbegin', `
-        <img class="center_image_exhib center_image_one_exhib" src="img/solo1.webp" alt="center_image">
-        `);
+        <img class="center_image_exhib center_image_one_exhib" src="${imgArr[0]}" usemap="#imagemap" alt="center_image">
+        `)
         imgContainer.insertAdjacentHTML('beforeend', `
         <p id="captions_desk" class="captions">${captionsArr[0]}</p>
-        `);
+        `)
     } catch (e) {
-        console.log(e);
+        console.log(e)
     }
 }
 
 getOneExhib(exhName).then(() => {
-    const image = document.querySelector('.center_image_exhib');
-    const captions = document.querySelector('#captions_desk');
-    const viewsCount = document.querySelector('#counter_num');
-    let counter = 0;
+    const image = document.querySelector('.center_image_exhib')
+    const captions = document.querySelector('#captions_desk')
+    const viewsCount = document.querySelector('#counter_num')
+    setTimeout(() => {
+        image.classList.add('fade')
+    }, 1)
+    setTimeout(() => {
+        captions.classList.add('fade_captions')
+    }, 500)
+    let counter = 0
     const nextImage = () => {
-        if (counter >= 0 && counter < imgArr.length - 1) {
-            counter++;
-            image.src = imgArr[counter];
-            captions.innerText = captionsArr[counter].replace(/\\n/g, '\n');
-            console.log(captionsArr[counter]);
-            viewsCount.innerText = ` ${counter + 1}`;
+        counter++
+        if (counter == imgArr.length) {
+            counter = 0
         }
+        image.classList.remove('fade')
+        captions.classList.remove('fade_captions')
+        image.src = imgArr[counter]
+        setTimeout(() => {
+            image.classList.add('fade')
+        }, 1)
+        setTimeout(() => {
+            captions.classList.add('fade_captions')
+        }, 700)
+        captions.innerText = captionsArr[counter].replace(/\\n/g, '\n')
+        viewsCount.innerText = ` ${counter + 1}`
     };
-    image.addEventListener('click', nextImage);
-}).catch((e) => { console.log(e.message); })
+    const prevImage = () => {
+        counter--
+        if (counter == -1) {
+            counter = imgArr.length - 1
+        }
+        image.classList.remove('fade')
+        captions.classList.remove('fade_captions')
+        image.src = imgArr[counter]
+        setTimeout(() => {
+            image.classList.add('fade')
+        }, 1)
+        setTimeout(() => {
+            captions.classList.add('fade_captions')
+        }, 700)
+        captions.innerText = captionsArr[counter].replace(/\\n/g, '\n')
+        viewsCount.innerText = ` ${counter + 1}`
+    }
+    image.onclick = (e) => {
+        let center = image.width / 2
+        if (e.offsetX > center) {
+            nextImage()
+        } else {
+            prevImage()
+        }
+    }
+}).catch((e) => { console.log(e.message) })
