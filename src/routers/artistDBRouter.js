88d387ff755efb3078cf20e8d10eedf41a98b257
dantsyt <dantsyt@gmail.com>
2023-10-13 02:06:42 +0300

    INFO image

--- src/routers/artistDBRouter.js
+++ src/routers/artistDBRouter.js
@@ -37,8 +37,27 @@ artistsDBRouter.post('/postArtist', async (req, res) => {
 
 artistsDBRouter.post('/editArtist', async (req, res) => {
     try {
-        await Artist.updateOne({ lastname: "șarpe" }, {
-            firstname: "mihail"
+        await Artist.updateOne({ fullname: "dianacepleanu" }, {
+            captions: ["ANCUȚA\noil on paper,  2018\n29 x 41 cm",
+                "ANCUȚA WITH BEADED NECKLACE\noil on paper,  2018\n29 x 41 cm",
+                "PEPINE\noil on paper,  2017\n25 X 25 cm",
+                "PEPINE\noil on paper,  2017\n17 X 25 cm",
+                "SELF-PORTRAIT\noil on cardboard, 2016\n76 x 40 cm",
+                "SELF-PORTRAIT\noil on cardboard, 2013\n70 x 100 cm",
+                "SELF-PORTRAIT\noil on canvas, 2016\n65 x 55 cm",
+                "SELF-PORTRAIT\noil on canvas, 2023\n33 x 41 cm",
+                "UNTITLED\noil on cardboard, 2003\n25 x 20 cm",
+                "UNTITLED\noil on cardboard, 2003\n25 x 15 cm",
+                "PORTRAIT\ntempera and green pen on paper, 2003\n21 x 20 cm",
+                "TWO\noil on paper, 2003\n30 x 40 cm",
+                "J. H.\noil on paper, 2018\n21 x 28 cm",
+                "KRISTINA\noil on cardboard, 2017\n27 x 24 cm",
+                "KRISTINA\noil on paper,  2017\n18 x 20 cm",
+                "IORGU\noil on canvas, 2016\n33 x 34 cm",
+                "IORGU (front)\noil on canvas, 2016\n33 x 34 cm",
+                "IORGU  (back)\noil on canvas, 2016\n30 x 33 cm",
+                "SELF-PORTRAIT\noil on canvas, 2013\n30 x 40 cm"
+            ]
         })
         res.status(201).send("Yeah bby")
     } catch (e) {
