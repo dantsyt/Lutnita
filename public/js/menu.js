74c06d682ed3ea61a853e34787b5f2bb72fc900f
dantsyt <dantsyt@gmail.com>
2023-10-24 01:37:04 +0300

    MENU disappear 3s

--- public/js/menu.js
+++ public/js/menu.js
@@ -6,6 +6,7 @@ const navBtns = document.querySelectorAll('.nav-btns');
 const mediaQueryList = window.matchMedia('only screen and (max-width: 1024px)');
 
 if (mediaQueryList.matches) {
+    navbar.className = 'navbar_hidden';
     const pageTitle = document.querySelector('h2');
     pageTitle.addEventListener('click', () => {
         navbar.className = 'navbar';
@@ -22,6 +23,9 @@ if (mediaQueryList.matches) {
         }
     })
 } else {
+    setTimeout(() => {
+        navbar.className = 'navbar_hidden'
+    }, 3000)
     const displayNavBar = () => {
         navbar.className = 'navbar';
         for (let one of navBtns) {
@@ -36,5 +40,30 @@ if (mediaQueryList.matches) {
         navBtns.className = 'nav-btns_hidden';
     });
 };
+
+(function (mouseStopDelay) {
+    var timeout;
+    document.addEventListener('mousemove', function (e) {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            var event = new CustomEvent("mousestop", {
+                detail: {
+                    clientX: e.clientX,
+                    clientY: e.clientY
+                },
+                bubbles: true,
+                cancelable: true
+            });
+            e.target.dispatchEvent(event);
+        }, mouseStopDelay);
+    });
+}(1000));
+
+// Example use
+document.querySelector('body').addEventListener('mousestop', function (e) {
+    console.log('You stopped your mouse while on the link');
+    console.log('Mouse coordinates are: ', e.detail.clientX, e.detail.clientY);
+    // The event will bubble up to parent elements.
+});
 
 document.oncontextmenu = () => { return false }
