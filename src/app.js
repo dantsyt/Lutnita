a8f14d80cd8a4ba192813b673c6d3989516a1da7
dantsyt <dantsyt@gmail.com>
2024-04-13 00:31:04 +0300

    UPCOMING Bodea; miscs

--- src/app.js
+++ src/app.js
@@ -42,8 +42,8 @@ app.use(exhibDBRouter)
 app.use(newsDBRouter)
 
 app.get('/', (req, res) => {
-    res.render('current', {
-        title: 'current'
+    res.render('upcoming', {
+        title: 'Upcoming'
     })
 })
 app.use('/artists', artistsRouter)
