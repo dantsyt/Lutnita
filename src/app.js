015b6feb1c021ac70ed9c16de402bbaf997cf311
dantsyt <dantsyt@gmail.com>
2024-11-09 00:20:48 +0200

    Jenkins webhook testing

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ app.use(morganMiddleware)
 app.get('/', (req, res) => {
     logger.info('Request for home page')
     res.render('upcoming', {
-        title: 'Push me'
+        title: 'Push me again'
     })
 })
 app.use('/artists', artistsRouter)
