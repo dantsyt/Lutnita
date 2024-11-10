58e8ecf4fa7de0ac00c1360b5837f026b540966e
dantsyt <dantsyt@gmail.com>
2024-11-10 20:09:10 +0200

    test successful

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ app.use(morganMiddleware)
 app.get('/', (req, res) => {
     logger.info('Request for home page')
     res.render('upcoming', {
-        title: 'Hei bubilicea'
+        title: 'Current'
     })
 })
 app.use('/artists', artistsRouter)
