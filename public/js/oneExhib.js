dc6182522891a2bed404085b0367b8f854be6d3e
dantsyt <dantsyt@gmail.com>
2024-12-27 21:49:15 +0200

    SWIPE prevent scroll improve 3.0

--- public/js/oneExhib.js
+++ public/js/oneExhib.js
@@ -119,12 +119,14 @@ getOneExhib(exhId).then(() => {
         const max_y = 40
         const max_time = 700
         let startTime = 0
+        let isHorizontalSwipe = false
 
         el.addEventListener('touchstart', function (e) {
             const t = e.touches[0]
             swipe_det.sX = t.screenX
             swipe_det.sY = t.screenY
             startTime = new Date().getTime()
+            isHorizontalSwipe = false
         }, false)
 
         el.addEventListener('touchmove', function (e) {
@@ -133,7 +135,10 @@ getOneExhib(exhId).then(() => {
             swipe_det.eY = t.screenY
             const dX = swipe_det.eX - swipe_det.sX
             if (Math.abs(dX) > min_x) {
-                e.preventDefault();
+                isHorizontalSwipe = true
+            }
+            if (isHorizontalSwipe) {
+                e.preventDefault()
             }
         }, false)
 
@@ -142,7 +147,7 @@ getOneExhib(exhId).then(() => {
             const dY = swipe_det.eY - swipe_det.sY
             const elapsedTime = new Date().getTime() - startTime
 
-            if (Math.abs(dX) > min_x && Math.abs(dY) < max_y && elapsedTime <= max_time) {
+            if (Math.abs(dX) > min_x && Math.abs(dY) < max_y && elapsedTime <= max_time && isHorizontalSwipe) {
                 if (dX > 0) {
                     func('right')
                 } else {
